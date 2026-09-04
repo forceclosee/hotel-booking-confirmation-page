434e424b2662494d61813288a0518e34e5495eea
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-09-04 11:41:02 +0700

    feat(layout): add mobile menu with focus trap

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,47 @@
+import { createSignal } from "solid-js";
+
 import SiteName from "#/components/shared/SiteName";
 import ThemeToggle from "#/components/shared/ThemeToggle";
+import Button from "#/components/ui/Button";
+import MobileMenu from "#/components/shared/MobileMenu";
 
 export default function Header() {
+	const [isMenuOpen, setIsMenuOpen] = createSignal(false);
+
+	// fallback event listener for browser that didn't support popover api with early return for supported browser
+	const handleMenuButtonClick = () => {
+		if ("popover" in HTMLElement.prototype) {
+			return;
+		}
+
+		setIsMenuOpen((prev) => !prev);
+	};
+
 	return (
-		<header class="lg:hidden flex gap-4 px-fluid-300 py-3.5 border-be border-border">
-			<SiteName />
-			<ThemeToggle class="ms-auto" />
-		</header>
+		<>
+			<header class="lg:hidden min-block-header-height flex items-center gap-4 px-fluid-300 border-be border-border">
+				<SiteName />
+				<ThemeToggle class="ms-auto" />
+				<Button
+					variant="secondary"
+					popoverTarget="mobile-menu-panel"
+					class="group block-11.5 inline-11.5 flex flex-col justify-center items-center gap-1 rounded-lg cursor-pointer shrink-0 squircle"
+					aria-expanded={isMenuOpen()}
+					aria-controls="mobile-menu-panel"
+					aria-label={isMenuOpen() ? "Close menu" : "Open menu"}
+					onclick={handleMenuButtonClick}>
+					<span
+						class="block-0.5 inline-3.5 bg-text group-[[aria-expanded='true']]:rotate-45 hamburger-transition-timing translate-y-0 group-[[aria-expanded='true']]:translate-y-1.5 pointer-events-none"
+						aria-hidden="true"></span>
+					<span
+						class="block-0.5 inline-3.5 bg-text group-[[aria-expanded='true']]:opacity-0 hamburger-transition-timing translate-y-0 pointer-events-none"
+						aria-hidden="true"></span>
+					<span
+						class="block-0.5 inline-3.5 bg-text group-[[aria-expanded='true']]:-rotate-45 hamburger-transition-timing translate-y-0 group-[[aria-expanded='true']]:-translate-y-1.5 pointer-events-none"
+						aria-hidden="true"></span>
+				</Button>
+			</header>
+			<MobileMenu open={isMenuOpen()} onToggle={setIsMenuOpen} />
+		</>
 	);
 }
---
+++ src/components/shared/MobileMenu.tsx
@@ -0,0 +1,63 @@
+import { createSignal, onCleanup, onMount } from "solid-js";
+import { Portal } from "solid-js/web";
+
+import createFocusTrap from "solid-focus-trap";
+
+import WeatherWidget from "#/components/shared/WeatherWidget";
+import FooterInfo from "#/components/shared/FooterInfo";
+
+type Props = {
+	open: boolean;
+	onToggle: (open: boolean) => void;
+};
+
+export default function MobileMenu(props: Props) {
+	const [mobileMenuRef, setMobileMenuRef] = createSignal<HTMLDivElement | null>(
+		null,
+	);
+
+	createFocusTrap({
+		element: mobileMenuRef,
+		enabled: () => props.open,
+	});
+
+	// synchronize popover state with signal
+	const handleToggle = (event: ToggleEvent) => {
+		const isPopoverOpen = event.newState === "open";
+
+		props.onToggle(isPopoverOpen);
+	};
+
+	// close sidebar on desktop screen
+	onMount(() => {
+		const desktopScreen = window.matchMedia("(width >= 64rem)");
+
+		const handleHidePopover = (event: MediaQueryListEvent) => {
+			if (event.matches) {
+				mobileMenuRef()?.hidePopover();
+			}
+		};
+
+		desktopScreen?.addEventListener("change", handleHidePopover);
+
+		onCleanup(() => {
+			desktopScreen?.removeEventListener("change", handleHidePopover);
+		});
+	});
+
+	return (
+		<Portal>
+			<div
+				ref={setMobileMenuRef}
+				id="mobile-menu-panel"
+				popover
+				class="block-[calc(100%_-_var(--spacing-header-height))] inline-full inset-bs-header-height [:popover-open]:flex justify-center bg-bg-page menu-transition"
+				onToggle={handleToggle}>
+				<div class="max-inline-[50rem] flex flex-col justify-end gap-4 px-fluid-300 py-4">
+					<WeatherWidget />
+					<FooterInfo />
+				</div>
+			</div>
+		</Portal>
+	);
+}
